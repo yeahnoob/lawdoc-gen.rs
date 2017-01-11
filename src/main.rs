af93b6caacd590875c13adbe078fa724bfb9aaa3
Xanadu Zhang <yeahnoob@buaa.edu.cn>
2017-01-11 13:09:29 +0800

    try the closures function of the rust-mastache's

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,47 @@ extern crate mustache;
 extern crate rustc_serialize;
 
 use std::io;
+use mustache::{MapBuilder, Template, Data};
 
 #[derive(RustcEncodable)]
 struct Planet {
     name: String,
+    sex: String,
+}
+
+fn data_stdout(tp: &Template, dp: &Data) {
+    println!("===");
+    tp.render_data(&mut io::stdout(), &dp).unwrap();
+    println!();
+}
+
+fn planet_stdout(tp: &Template, dp: &Planet) {
+    println!("===");
+    tp.render(&mut io::stdout(), &dp).unwrap();
+    println!();
 }
 
 fn main() {
-    let temp = mustache::compile_str("您好 {{name}}").unwrap();
-    let planet = Planet { name: "刘於凤".into() };
+    // read the template
+    let tp = mustache::compile_str("用户: {{name}}\n性别: {{sex}}").unwrap();
+    // fill contents
+    let planet = Planet {
+        name: "刘於凤".into(),
+        sex: "女".into(),
+    };
+    // get result from the above contents & template
     println!(" * 生成结果 *");
-    temp.render(&mut io::stdout(), &planet).unwrap();
-    println!();
+    planet_stdout(&tp, &planet);
+
+    // closures
+    let mut names = vec!["甲", "乙", "丙"];
+    let mut sexs = vec!["男", "男", "女"];
+    let data = MapBuilder::new()
+        .insert_fn("name", move |_| names.pop().unwrap().into())
+        .insert_fn("sex", move |_| sexs.pop().unwrap().into())
+        .build();
+
+    data_stdout(&tp, &data);
+    data_stdout(&tp, &data);
+    data_stdout(&tp, &data);
 }
